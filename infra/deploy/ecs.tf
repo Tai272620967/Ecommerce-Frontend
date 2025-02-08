821aa0c14d7dfe24b75ffc76a06d30dfca9c0651
TaiDT <taidt@splus-software.com.vn>
2025-02-08 14:51:58 +0700

    change port ecs

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -176,8 +176,8 @@ resource "aws_security_group" "ecs_front_service" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
