9ede19a5860743553fb7e6db9b287389353dfe78
TaiDT <taidt@splus-software.com.vn>
2025-02-11 22:11:56 +0700

    update code to deploy by Amplify

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // ⚡ Xuất site tĩnh (bắt buộc để chạy trên Amplify)
   reactStrictMode: false, // Tắt Strict Mode để kiểm tra lỗi gọi API 2 lần
+  trailingSlash: true, // ⚡ Giúp đường dẫn hoạt động đúng trên Amplify
   images: {
     domains: ['localhost'], // Thêm domain của server backend
+    unoptimized: true, // ⚡ Bắt buộc vì `next export` không hỗ trợ tối ưu ảnh
   },
   webpack(config) {
     config.module.rules.push({
